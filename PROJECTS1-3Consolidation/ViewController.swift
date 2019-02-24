040fd485bf1e4a7eaa6e270e0fa4cf1230b2235c
saman <abdallaelsaman67@gmail.com>
2019-02-24 18:21:49 +0200

    loading pictures from bundle

--- PROJECTS1-3Consolidation/ViewController.swift
+++ PROJECTS1-3Consolidation/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix("@3x.png") {
+                pictures.append(item)
+            }
+        }
+        for picture in pictures {
+            print(picture)
+        }
     }
 
 
